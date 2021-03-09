5d2eceeca22613aa9ff7c4fbb3938e3e7886efdd
davcapimb <269593@studenti.unimore.it>
2021-03-09 15:49:14 +0100

    lista nella dashboard

--- utility/openDB.py
+++ utility/openDB.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect("db.sqlite3")
+connection = sqlite3.connect("../database/db.sqlite3")
 cursor = connection.cursor()
 # cursor.execute("INSERT INTO Apiary VALUES ('100', 'vg')") # clear table
 cursor.execute("SELECT * FROM SensorFeed")  # clear table
