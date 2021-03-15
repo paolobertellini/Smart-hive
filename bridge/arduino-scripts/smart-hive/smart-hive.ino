b9e9c3891632918b2ab887ec98c5a670fd8e3047
paolobertellini <225184@studenti.unimore.it>
2021-03-15 19:17:50 +0100

    Arduino e bridge new

--- bridge/arduino-scripts/smart-hive/smart-hive.ino
+++ bridge/arduino-scripts/smart-hive/smart-hive.ino
@@ -14,12 +14,11 @@ const int tempPin = 2;
 const int buzzerPin = 8;
 
 //fsm
-int iState; //IDLE
+int iState;
 //serial
-unsigned long lasttime;
-String error;
+String in_buffer;
 //buzzer
-int duration = 500;  // 500 miliseconds
+int duration = 500;  //miliseconds
 bool alarm = false;
 //weight
 HX711_ADC LoadCell(weight_dataPin, weight_sckPin);
@@ -37,16 +36,168 @@ float hum = 0.0;
 float temp = 0.0;
 //json
 const size_t CAPACITY = JSON_OBJECT_SIZE(7);
-StaticJsonDocument<CAPACITY> jdata;
-StaticJsonDocument<CAPACITY> jerr;
-JsonObject jsonData = jdata.to<JsonObject>();
-JsonObject jsonerr = jerr.to<JsonObject>();
+StaticJsonDocument<CAPACITY> output;
+JsonObject jsonData = output.to<JsonObject>();
+StaticJsonDocument<200> input;
 //eeprom
-int eeAddressId=0;
+int eeAddressId = 0;
 int eeAddressCal = 6;
 char saved_id[6] = "none";
 char new_id[6];
-String old_id(saved_id);
+String id = String(saved_id);
+
+#define NOTE_B0  31
+#define NOTE_C1  33
+#define NOTE_CS1 35
+#define NOTE_D1  37
+#define NOTE_DS1 39
+#define NOTE_E1  41
+#define NOTE_F1  44
+#define NOTE_FS1 46
+#define NOTE_G1  49
+#define NOTE_GS1 52
+#define NOTE_A1  55
+#define NOTE_AS1 58
+#define NOTE_B1  62
+#define NOTE_C2  65
+#define NOTE_CS2 69
+#define NOTE_D2  73
+#define NOTE_DS2 78
+#define NOTE_E2  82
+#define NOTE_F2  87
+#define NOTE_FS2 93
+#define NOTE_G2  98
+#define NOTE_GS2 104
+#define NOTE_A2  110
+#define NOTE_AS2 117
+#define NOTE_B2  123
+#define NOTE_C3  131
+#define NOTE_CS3 139
+#define NOTE_D3  147
+#define NOTE_DS3 156
+#define NOTE_E3  165
+#define NOTE_F3  175
+#define NOTE_FS3 185
+#define NOTE_G3  196
+#define NOTE_GS3 208
+#define NOTE_A3  220
+#define NOTE_AS3 233
+#define NOTE_B3  247
+#define NOTE_C4  262
+#define NOTE_CS4 277
+#define NOTE_D4  294
+#define NOTE_DS4 311
+#define NOTE_E4  330
+#define NOTE_F4  349
+#define NOTE_FS4 370
+#define NOTE_G4  392
+#define NOTE_GS4 415
+#define NOTE_A4  440
+#define NOTE_AS4 466
+#define NOTE_B4  494
+#define NOTE_C5  523
+#define NOTE_CS5 554
+#define NOTE_D5  587
+#define NOTE_DS5 622
+#define NOTE_E5  659
+#define NOTE_F5  698
+#define NOTE_FS5 740
+#define NOTE_G5  784
+#define NOTE_GS5 831
+#define NOTE_A5  880
+#define NOTE_AS5 932
+#define NOTE_B5  988
+#define NOTE_C6  1047
+#define NOTE_CS6 1109
+#define NOTE_D6  1175
+#define NOTE_DS6 1245
+#define NOTE_E6  1319
+#define NOTE_F6  1397
+#define NOTE_FS6 1480
+#define NOTE_G6  1568
+#define NOTE_GS6 1661
+#define NOTE_A6  1760
+#define NOTE_AS6 1865
+#define NOTE_B6  1976
+#define NOTE_C7  2093
+#define NOTE_CS7 2217
+#define NOTE_D7  2349
+#define NOTE_DS7 2489
+#define NOTE_E7  2637
+#define NOTE_F7  2794
+#define NOTE_FS7 2960
+#define NOTE_G7  3136
+#define NOTE_GS7 3322
+#define NOTE_A7  3520
+#define NOTE_AS7 3729
+#define NOTE_B7  3951
+#define NOTE_C8  4186
+#define NOTE_CS8 4435
+#define NOTE_D8  4699
+#define NOTE_DS8 4978
+
+int melody[] = {
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_AS4, NOTE_AS4, NOTE_AS4, NOTE_AS4,
+  NOTE_D5, NOTE_D5, NOTE_D5, NOTE_D5,
+  NOTE_C5, NOTE_C5, NOTE_C5, NOTE_C5, 
+  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5, 
+  NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5,
+  NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5, 
+  NOTE_G5, NOTE_G5, NOTE_G5, NOTE_G5, 
+  NOTE_C5, NOTE_AS4, NOTE_A4, NOTE_F4,
+  NOTE_G4, 0, NOTE_G4, NOTE_D5,
+  NOTE_C5, 0, NOTE_AS4, 0,
+  NOTE_A4, 0, NOTE_A4, NOTE_A4,
+  NOTE_C5, 0, NOTE_AS4, NOTE_A4, 
+  NOTE_G4,0, NOTE_G4, NOTE_AS5,
+  NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5,
+  NOTE_G4,0, NOTE_G4, NOTE_AS5,
+  NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5,
+  NOTE_G4, 0, NOTE_G4, NOTE_D5,
+  NOTE_C5, 0, NOTE_AS4, 0,
+  NOTE_A4, 0, NOTE_A4, NOTE_A4,
+  NOTE_C5, 0, NOTE_AS4, NOTE_A4, 
+  NOTE_G4,0, NOTE_G4, NOTE_AS5,
+  NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5,
+  NOTE_G4,0, NOTE_G4, NOTE_AS5,
+  NOTE_A5, NOTE_AS5, NOTE_A5, NOTE_AS5
+ };
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  4,4,4,4,
+  };
 
 void setup() {
   // initialize serial communications
@@ -54,15 +205,14 @@ void setup() {
 
   //id
   EEPROM.get(eeAddressId, saved_id);
-  old_id= String(saved_id);
-  
+  id = String(saved_id);
+
   //led
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
 
   //fsm
   iState = 0;
-  lasttime = millis();
 
   //temp e hum
 
@@ -80,91 +230,127 @@ void setup() {
   startMillis = millis();
 }
 
-void loop() { //0 IDLE 1 WRITE-MES 2 WRITE-DATA 3 READ
+void loop() { //0 IDLE
+  //1 READ
+  //2 CHECK
+  //3 AUTHENTICATION
+  //4 DATA
+  //5 READ SENSORS
 
 
-  //3: read
+  //IDLE --> READ
   if (iState == 0 && Serial.available() > 0) {
-    iState = 3;
-    String received = Serial.readStringUntil('\n');
-
-    StaticJsonDocument<200> doc;
-    DeserializationError error = deserializeJson(doc, received);
-    if (error) {
-      iState = 1;
-      jerr["type"] = "E";
-      jerr["desc"] = "Bridge communication error";
-    }
-
-    if (doc["type"] == "A") {
-      String arrived_id = doc["id"];
-      arrived_id.toCharArray(new_id, 6);
-      if (arrived_id != old_id){
-        EEPROM.put(eeAddressId, new_id);
-        old_id = arrived_id;
-      }
-    }
-    if (doc["type"] == "C") {
-      if (doc["entrance"] == "True" && entrance == false) {
-        openServo();
-        entrance = true;
-      }
-      if (doc["entrance"] == "False" && entrance == true) {
-        closeServo();
-        entrance = false;
-      }
-      if (doc["alarm"] == "True") {
-        int d = doc["duration"];
-        tone(8, NOTE_C3, d);
-        doc["alarm"] = "False";
-      }
-    }
-
-    iState = 0;
-  }
-
-  //2: write data
-  if (iState == 0 && (millis() - lasttime) > 2000) {
-    lasttime = millis();
-
-    //temp and hum
-    byte data[40] = {0};
-    if (dht11.read2(tempPin, &temp, &hum, data)) {
-      iState = 1;
-      jerr["type"] = "E";
-      jerr["desc"] = "Temperature and humudity sensor error";
-    }
-    else {
+    iState = 1;
+    char received = Serial.read();
+    if (received == '\n') {
       iState = 2;
     }
-    if (!LoadCell.update()) {
-      iState = 1;
-      jerr["type"] = "E";
-      jerr["desc"] = "Weight sensor error";
-    }
-    weight = LoadCell.getData();
-
-    if (iState == 2) {
-      jdata["type"] = "D";
-      jdata["hive_id"] = old_id;
-      jdata["humidity"] = hum;
-      jdata["temperature"] = temp;
-      jdata["weight"] = weight;
-      jdata["association_code"] = 22222;
+    else {
+      in_buffer += received;
       iState = 0;
-      serializeJson(jdata, Serial);
-      Serial.println();
-
     }
   }
 
-  if (iState == 1) {
-    serializeJson(jerr, Serial);
-    Serial.println();
+  //READ --> CHECK
+  if (iState == 2) {
+    if (in_buffer.endsWith("}") && in_buffer.startsWith("{")) {
+      
+      DeserializationError error = deserializeJson(input, in_buffer);
+      if (error) {
+        in_buffer = "";
+        iState = 0;
+      }
+      else if (input["type"] == "A") {
+        iState = 3;
+      }
+      else if (input["type"] == "D") {
+        iState = 4;
+      }
+      else {
+        in_buffer = "";
+        iState = 0;
+      }
+    }
+    else {
+      in_buffer = "";
+      iState = 0;
+    }
+  }
+
+  //CHECK --> AUTHENTICATION
+  if (iState == 3) {
+    if (input["id"] == "None") {
+      output["type"] = "A";
+      output["association_code"] = 33333;
+      output["id"] = "None";
+      serializeJson(output, Serial);
+      Serial.println();
+    }
+    else {
+      String arrived_id = input["id"];
+      arrived_id.toCharArray(new_id, 6);
+      EEPROM.put(eeAddressId, new_id);
+    }
     iState = 0;
+    in_buffer = "";
+  }
+
+  //CHECK --> DATA
+  if (iState == 4) {
+    if (input["entrance"] == "True" && entrance == false) {
+      openServo();
+      entrance = true;
+    }
+    if (input["entrance"] == "False" && entrance == true) {
+      closeServo();
+      entrance = false;
+    }
+    if (input["alarm"] == "True") {
+      duration = input["duration"];
+      alarm = true;
+    }
+    if (input["alarm"] == "False") {
+      alarm = false;
+    }
+
+    if (input["data"] == "True") {
+      byte data[40] = {0};
+      output["type"] = "D";
+      output["id"] = id;
+      if (dht11.read2(tempPin, &temp, &hum, data)) {
+        output["humidity"] = "error";
+        output["temperature"] = "error";
+      }
+      else {
+        output["humidity"] = hum;
+        output["temperature"] = temp;
+      }
+      if (!LoadCell.update()) {
+        output["weight"] = "error";
+      }
+      else {
+        output["weight"] = LoadCell.getData();
+      }
+      serializeJson(output, Serial);
+      Serial.println();
+    }
+    iState = 0;
+    in_buffer = "";
+  }
+
+  if (alarm) {
+    for (int thisNote = 0; thisNote < 112; thisNote++) {
+
+    int noteDuration = 750 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    
+    noTone(8);
+  }
   }
 }
-
 
 void openServo() {
   currentMillis = millis();
--- bridge/bridge.py
+++ bridge/bridge.py
@@ -5,134 +5,196 @@ import requests
 import serial
 import serial.tools.list_ports
 
+import _thread
+import sys
+
 online = 'http://smart-hive.ddns.net:8080'
 local = 'http://127.0.0.1:8080'
 davide = 'http://localhost'
 
 server = online
+ports_descriptions = ["arduino", "usb", "tty"]
 
 
+def setup():
 
-class FBridge():
-    def setup(self):
-        self.ser = None
-        self.portname = None
-        try:
-            # print("Available ports: ")
-            ports = serial.tools.list_ports.comports()
-            for port in ports:
-                # print("DEVICE: " + str(port.device))
-                # print("DESCRIPTION: " + str(port.description))
-                if 'arduino' in port.description.lower():
-                    self.portname = port.device
-            # print("Trying to connect to: " + self.portname)
-        except:
-            print("BRIDGE UNABLE TO FIND SERIAL PORT")
-
-        try:
-            if self.portname is not None:
-                self.ser = serial.Serial(self.portname, 9600, timeout=0)
-                print("BRIDGE SUCCESSFULLY CONNECTED TO: " + str(self.portname))
-        except:
-            self.ser = None
-            print("BRIDGE UNABLE TO CONNECT TO SERIAL PORT")
-
-        try:
-            r = requests.get(server + '/test')
-            if r.text == "200":
-                print("BRIDGE SUCCESSFULLY TEST SERVER CONNECTION TO: " + server)
-            else:
-                print("BRIDGE UNABLE TO CONNECT TO SERVER")
-        except:
-            print("BRIDGE UNABLE TO CONNECT TO SERVER")
-
-    def loop(self, updateInterval = 10, hiveFeedInterval = 5):
-
-        hiveFeedTime = time.time()
-        updateTime = time.time()
-
-        self.inbuffer = ""
-        self.data = None
-        self.hive_id = None
-        # self.ser.flush()
-
+    try:
         print("Setting up bridge..")
+        ports = []
+        print("Available ports: ")
+        available_ports = serial.tools.list_ports.comports()
+        for port in available_ports:
+            print("Device: " + str(port.device) + " Description: " + str(port.description))
+            for desc in ports_descriptions:
+                if desc in port.description.lower():
+                    ports.append(port)
+        # print("Trying to connect to: " + self.portname)
+        return ports
+    except:
+        print("BRIDGE UNABLE TO FIND SERIAL PORTS")
 
-        while (True):
-            if self.ser is not None and self.ser.in_waiting > 0:
-                lastchar = self.ser.read().decode('utf-8')
-                if lastchar == '\n':
-                    print("Bridge ready")
+
+def loop(threadName, port, updateInterval = 10):
+
+    buffer=""
+
+    try:
+        if port.device is not None:
+            serial_port = serial.Serial(port.device, 9600, timeout=0)
+            print("BRIDGE SUCCESSFULLY CONNECTED TO: " + str(port))
+
+    except Exception as e:
+        serial_port = None
+        print("ERROR: bridge unable to connect to " + str(port.name))
+        print(e)
+        raise
+
+    try:
+        command = "{\"type\":\"A\",\"association_code\":\"None\",\"id\":\"None\"}"
+        serial_port.write(command.encode())
+        serial_port.write(b'\n')
+        print("B-->MC: association code request")
+    except Exception as e:
+        print("ERROR: bridge unable to ask association code to the microcontroller")
+        print(e)
+        raise
+
+
+    while (True):
+        if serial_port is not None and serial_port.in_waiting > 0:
+            try:
+                lastchar = serial_port.read().decode('utf-8')
+                if lastchar != '\n':  # EOF
+                    buffer += lastchar
+                else:
                     break
+            except Exception as e:
+                print("ERROR: bridge unable to read character from serial")
+                print(e)
+                raise
+    try:
+        # print("BRIDGE RICEVE RAW DATA: " + self.inbuffer)
+        received = json.loads(buffer)
+        print("MC-->B: association code answer")
+        buffer = ""
+    except Exception as e:
+        print("ERROR: bridge unable to read json")
+        print(e)
+        raise
 
-        while (True):
-            if self.ser is not None and self.ser.in_waiting > 0:
+    try:
+        association_code = {"association_code":received["association_code"]}
+        r = requests.get(server + '/authentication', json=association_code)
+        if r.text != "None":
+            id = r.text
+            print("C-->B: received id " + id)
+        else:
+            print("ERROR: bridge unable to receive id from server")
+    except Exception as e:
+        print("ERROR: bridge unable to connect to " + server)
+        print(e)
+        raise
 
-                try:
-                    lastchar = self.ser.read().decode('utf-8')
+    try:
+        command = "{\"type\":\"A\",\"association_code\":\"" + str(received["association_code"]) + "\",\"id\":\"" + str(id) + "\"}"
+        serial_port.write(command.encode())
+        serial_port.write(b'\n')
+        print("B-->MC: authentication id " + id)
+    except Exception as e:
+        print("ERROR: bridge unable to ask association code to the microcontroller")
+        print(e)
+        raise
 
-                    if lastchar == '\n':  # EOF
-                        try:
-                            # print("BRIDGE RICEVE RAW DATA: " + self.inbuffer)
-                            self.data = json.loads(self.inbuffer)
-                            self.inbuffer = ""
-                        except:
-                            print("ATTENTION! bridge unable to interpret json")
-
-                        # update hive status
-                        if time.time() > updateTime + updateInterval and self.data is not None:
-                            try:
-                                if self.hive_id is not None:
-                                    id = {"id": self.hive_id}
-                                    r = requests.get(server + '/bridge-channel', json=id)
-                                    ser_resp = json.loads(r.text)
-                                    duration = 500
-                                    hiveFeedInterval = ser_resp["update_freq"]
-                                    print(hiveFeedInterval)
-                                    json_comando = "{\"type\":\"C\",\"entrance\":\"" + str(
-                                        ser_resp["entrance"]) + "\", \"alarm\":\"" + str(
-                                        ser_resp["alarm"]) + "\", \"duration\":\"" + str(duration) + "\"}"
-                                    print("B --> A: " + json_comando)
-
-                                    self.ser.write(json_comando.encode())
-                                    self.ser.write(b'\n')
-                                    updateTime = time.time()
-
-                            except:
-                                print("ATTENTION! Bridge unable to receive data from server and update hive status")
-
-                        # hive feed
-                        if time.time() > hiveFeedTime + hiveFeedInterval and self.data is not None:
-                            try:
-                                if self.data["type"] == "D":
-                                    print("A --> B [DATA]: " + str(self.data))
-                                    r1 = requests.get(server + '/new-sensor-feed', json=self.data)
-                                    ser_resp = json.loads(r1.text)
-                                    if ser_resp["hive_id"] is not None:
-                                        json_id = "{\"type\":\"A\",\"id\":\"" + str(ser_resp["hive_id"]) + "\"}"
-                                        self.hive_id = str(ser_resp["hive_id"])
-                                        self.ser.write(json_id.encode())
-                                        self.ser.write(b'\n')
-                                        # print("RECEIVED HIVE ID: " + json_id)
-                                    else:
-                                        print("ATTENTION! Hive not autenticated on the server")
-                                elif (self.data["type"] == "E"):
-                                    print("A --> B [ERROR]: " + self.data["desc"])
-                                else:
-                                    print("ATTENTION! Communication error")
-
-                                hiveFeedTime = time.time()
-
-                            except:
-                                print("ATTENTION! Bridge unable to send hive feed data to the server")
-
-                    else:
-                        self.inbuffer = self.inbuffer + lastchar
-
-                except:
-                    pass
+    #
+    #
+    #     hiveFeedTime = time.time()
+    #     updateTime = time.time()
+    #
+    #     self.inbuffer = ""
+    #     self.data = None
+    #     self.hive_id = None
+    #     # self.ser.flush()
+    #
+    #     print("Setting up bridge..")
+    #
+    #     while (True):
+    #         if self.ser is not None and self.ser.in_waiting > 0:
+    #             lastchar = self.ser.read().decode('utf-8')
+    #             if lastchar == '\n':
+    #                 print("Bridge ready")
+    #                 break
+    #
+    #     while (True):
+    #         if self.ser is not None and self.ser.in_waiting > 0:
+    #
+    #             try:
+    #                 lastchar = self.ser.read().decode('utf-8')
+    #
+    #                 if lastchar == '\n':  # EOF
+    #                     try:
+    #                         # print("BRIDGE RICEVE RAW DATA: " + self.inbuffer)
+    #                         self.data = json.loads(self.inbuffer)
+    #                         self.inbuffer = ""
+    #                     except:
+    #                         print("ATTENTION! bridge unable to interpret json")
+    #
+    #                     # update hive status
+    #                     if time.time() > updateTime + updateInterval and self.data is not None:
+    #                         try:
+    #                             if self.hive_id is not None:
+    #                                 id = {"id": self.hive_id}
+    #                                 r = requests.get(server + '/bridge-channel', json=id)
+    #                                 ser_resp = json.loads(r.text)
+    #                                 duration = 500
+    #                                 hiveFeedInterval = ser_resp["update_freq"]
+    #                                 print(hiveFeedInterval)
+    #                                 json_comando = "{\"type\":\"C\",\"entrance\":\"" + str(
+    #                                     ser_resp["entrance"]) + "\", \"alarm\":\"" + str(
+    #                                     ser_resp["alarm"]) + "\", \"duration\":\"" + str(duration) + "\"}"
+    #                                 print("B --> A: " + json_comando)
+    #
+    #                                 self.ser.write(json_comando.encode())
+    #                                 self.ser.write(b'\n')
+    #                                 updateTime = time.time()
+    #
+    #                         except:
+    #                             print("ATTENTION! Bridge unable to receive data from server and update hive status")
+    #
+    #                     # hive feed
+    #                     if time.time() > hiveFeedTime + hiveFeedInterval and self.data is not None:
+    #                         try:
+    #                             if self.data["type"] == "D":
+    #                                 print("A --> B [DATA]: " + str(self.data))
+    #                                 r1 = requests.get(server + '/new-sensor-feed', json=self.data)
+    #                                 ser_resp = json.loads(r1.text)
+    #                                 if ser_resp["hive_id"] is not None:
+    #                                     json_id = "{\"type\":\"A\",\"id\":\"" + str(ser_resp["hive_id"]) + "\"}"
+    #                                     self.hive_id = str(ser_resp["hive_id"])
+    #                                     self.ser.write(json_id.encode())
+    #                                     self.ser.write(b'\n')
+    #                                     # print("RECEIVED HIVE ID: " + json_id)
+    #                                 else:
+    #                                     print("ATTENTION! Hive not autenticated on the server")
+    #                             elif (self.data["type"] == "E"):
+    #                                 print("A --> B [ERROR]: " + self.data["desc"])
+    #                             else:
+    #                                 print("ATTENTION! Communication error")
+    #
+    #                             hiveFeedTime = time.time()
+    #
+    #                         except:
+    #                             print("ATTENTION! Bridge unable to send hive feed data to the server")
+    #
+    #                 else:
+    #                     self.inbuffer = self.inbuffer + lastchar
+    #
+    #             except:
+    #                 pass
 
 if __name__ == '__main__':
-    br = FBridge()
-    br.setup()
-    br.loop()
+
+    ports = setup()
+    # for port in ports:
+    #     _thread.start_new_thread(loop, ("Hive-1", port))
+    #     print("ERROR: unable to start thread")
+    loop("paolo", ports[0])
